fc333e77bc6eeeef53472db572305105b1dc8b80
brabbitdousha <1216874964@qq.com>
2022-03-16 16:54:51 +0800

    update

--- cuda/main.cu
+++ cuda/main.cu
@@ -5,6 +5,7 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include "vec3.h"
+#include "ray.h"
 
 #define checkCudaErrors(val) check_cuda((val),#val,_FILE_,_LINE_)
 
@@ -20,13 +21,22 @@ void check_cuda(cudaError_t result, char const* const func, const char* const fi
     }
 }
 
-__global__ void render(vec3* fb, int max_x, int max_y)
+__device__ vec3 color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5f * (unit_direction.y() + 1.0f);
+    return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
+__global__ void render(vec3* fb, int max_x, int max_y, vec3 lower_left_corner, vec3 horizontal, vec3 vertical, vec3 origin)
 {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if ((i >= max_x) || (j >= max_y)) return;
     int pixel_index = j * max_x + i;
-    fb[pixel_index] = vec3(float(i) / max_x, float(j) / max_y, 0.2f);
+    float u = float(i) / float(max_x);
+    float v = float(j) / float(max_y);
+    ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+    fb[pixel_index] = color(r);
   
 }
 
@@ -54,7 +64,10 @@ int main() {
     //block
     dim3 blocks(image_width / tx + 1, image_height / ty + 1);
     dim3 threads(tx, ty);
-    render <<<blocks, threads >>> (fb, image_width, image_height);
+    render <<<blocks, threads >>> (fb, image_width, image_height,vec3(-2.0, -1.0, -1.0),
+        vec3(4.0, 0.0, 0.0),
+        vec3(0.0, 2.0, 0.0),
+        vec3(0.0, 0.0, 0.0));
    cudaGetLastError();
     cudaDeviceSynchronize();
 
--- nocuda/main.cpp
+++ nocuda/main.cpp
@@ -1,42 +1,47 @@
-#include <iostream>
-#include <fstream>
-#include <time.h>
-#include <cstdio>
+#include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
+#include <iostream>
+
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
+
 int main() {
-    
+
     // Image
-    int image_width = 1200;
-    int image_height = 600;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
 
     // Render
-    FILE* fp = fopen("output.ppm", "wb");
-    fprintf(fp, "P3\n%d %d\n255\n",image_width,image_height);
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-    //timer
-   clock_t start, stop;
 
-    start = clock();
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-    for (int j = image_height - 1; j >= 0; --j) {
-    	std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+    for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r=double(i)/(image_width-1);
-            auto g=double(j)/(image_height-1);
-            auto b=0.25;
-            
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-    
-            fprintf(fp,"%d %d %d\n",ir,ig,ib);
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
         }
     }
-    //timer
-     stop = clock();
-    double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
-    std::cerr << "took " << timer_seconds << " seconds.\n";
 
-    fclose(fp);
+    std::cerr << "\nDone.\n";
 }
